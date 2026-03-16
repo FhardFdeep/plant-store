91b558d66a1a8481e9e81d6929648d55da477aa7
FhardFdeep <ninja107108@outlook.com>
2026-03-16 19:30:08 +0530

    fix products page

---
+++ app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductsPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>All Plants</h1>
+      <p>Here you will see all products.</p>
+    </div>
+  )
+}
